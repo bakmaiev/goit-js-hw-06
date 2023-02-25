016cb1637bf2e2789b048e6e33936531f48cc732
bakmaiev <thisyoungrhyme@gmail.com>
2023-02-25 16:19:22 +0200

    task-4 done

---
+++ js/task-04.js
@@ -0,0 +1,18 @@
+let counterValue = 0;
+const decrementBtnEl = document.querySelector('button[data-action="decrement"]');
+const incrementBtnEl = document.querySelector('button[data-action="increment"]');
+const valueEl = document.querySelector('#value');
+
+const handleDecrementClick = () => {
+    counterValue -= 1;
+    valueEl.textContent = counterValue;
+};
+
+decrementBtnEl.addEventListener('click', handleDecrementClick);
+
+const handleIncrementClick = () => {
+    counterValue += 1;
+    valueEl.textContent = counterValue;
+};
+
+incrementBtnEl.addEventListener('click', handleIncrementClick);
