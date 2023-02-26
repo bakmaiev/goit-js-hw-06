27e3ddfb668a49ea3076a8de6b7fbb12c0d1d158
bakmaiev <thisyoungrhyme@gmail.com>
2023-02-26 20:01:35 +0200

    task-09 done

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnEl = document.querySelector('.change-color');
+const textEl = document.querySelector('.color');
+
+const handleBtn = (e) => {
+  document.body.style.backgroundColor = `${getRandomHexColor()}`;
+  textEl.textContent = `${getRandomHexColor()}`
+};
+
+
+btnEl.addEventListener('click', handleBtn);
