fd159edcad8a6f28b1a9517ebdb9457676d6f20d
bakmaiev <thisyoungrhyme@gmail.com>
2023-02-27 18:30:49 +0200

    update task-06

--- js/task-06.js
+++ js/task-06.js
@@ -1,10 +1,8 @@
 const inputEl = document.querySelector('#validation-input');
 const validValue = inputEl.dataset.length; 
 const handleInput = (e) => {
-    e.currentTarget.classList.remove('valid', 'invalid');
-    e.currentTarget.value.length === Number(validValue)
-    ? e.currentTarget.classList.add('valid')
-    : e.currentTarget.classList.add('invalid');
+    e.currentTarget.classList.remove('valid', 'invalid'); 
+    e.currentTarget.classList.add( e.currentTarget.value.length === Number(validValue) ? 'valid' : 'invalid' )
     
     // TOGGLE METHOD
     // e.currentTarget.classList.toggle('valid', e.currentTarget.value.length === Number(validValue));
