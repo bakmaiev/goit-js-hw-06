ad194a853503f118fc92fe3609a25a407a1381df
bakmaiev <thisyoungrhyme@gmail.com>
2023-02-27 18:43:27 +0200

    update task-08

--- js/task-08.js
+++ js/task-08.js
@@ -5,14 +5,15 @@ const handleForm = (e) => {
 
     const { elements: { email, password } } = e.currentTarget;
     if (email.value === "" || password.value === "") alert("Всі поля мають бути заповнені. Спробуй ще раз, ти зможеш!");
+    else {
+        const dataForm = {
+            Email: email.value,
+            Password: password.value
+        }
 
-    const dataForm = {
-    Email: email.value,
-    Password: password.value
-}
-
-    console.log(dataForm);
-    e.currentTarget.reset();
+        console.log(dataForm);
+        e.currentTarget.reset();
+    }
 };
 
 formEl.addEventListener('submit', handleForm);
